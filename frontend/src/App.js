08c66f0976afd55d402c28ff52fe8d1e001fef31
kavantan <kavantan10@gmail.com>
2023-02-14 05:11:55 +0800

    Create placeholder GamePage for client

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,16 @@
-function App() {
-  return <div>Hello World!</div>;
-}
+import GamePage from "./pages/GamePage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<GamePage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
