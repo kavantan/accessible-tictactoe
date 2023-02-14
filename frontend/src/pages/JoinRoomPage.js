dcafeff3c8f395c73758aa69c34899a1e7083266
kavantan <kavantan10@gmail.com>
2023-02-14 16:45:49 +0800

    Edit JoinRoomPage to accept roomId and redirect to GamePage

--- frontend/src/pages/JoinRoomPage.js
+++ frontend/src/pages/JoinRoomPage.js
@@ -1,29 +1,83 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import "./JoinRoomPage.css";
 
-const JoinRoomPage = ({ socket }) => {
-  const [userName, setUserName] = useState("");
-  const [roomName, setRoomName] = useState("");
+const JoinRoom = ({ socket }) => {
+  const { user } = useSelector((state) => state.user);
+
+  const [joined, setJoined] = useState(false);
+
+  const [roomId, setRoomId] = useState("");
+  const [error, setError] = useState("");
+
+  const handleClick = () => {
+    if (roomId.length === 0) {
+      setError("Please enter room id");
+      setTimeout(() => {
+        setError("");
+      }, 4000);
+      return;
+    }
+
+    socket.emit("joinExistingRoom", {
+      username: user.userName,
+      userId: user.userId,
+      roomId,
+    });
+  };
+
+  useEffect(() => {
+    if (!user) {
+      window.location.href = "/";
+    }
+
+    socket.on("message", (payload) => {
+      console.log(payload);
+      if (payload.error) {
+        setError(payload.error);
+        setTimeout(() => {
+          setError("");
+        }, 4000);
+      } else {
+        setJoined(true);
+      }
+    });
+  });
 
   return (
-    <div>
-      <h1>Join A Room</h1>
-      <form>
-        <input
-          type="text"
-          placeholder="Your Name"
-          value={userName}
-          onChange={(e) => setUserName(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Room Name"
-          value={roomName}
-          onChange={(e) => setRoomName(e.target.value)}
-        />
-        <button>Join Room</button>
-      </form>
+    <div className="join-room">
+      <h1>Join Game</h1>
+
+      <div className="join-room-container">
+        <div className="url-container">
+          {error.length > 0 ? <p className="error">{error}</p> : null}
+
+          <input
+            value={roomId}
+            onChange={(e) => setRoomId(e.target.value)}
+            className="name-input"
+            type="text"
+          />
+
+          <button
+            disabled={joined}
+            onClick={handleClick}
+            className="room-btn copy-btn"
+          >
+            {joined ? "Joined" : "Join"}
+          </button>
+        </div>
+        <div className="go-to-game">
+          {joined ? (
+            <Link to={`/game/${roomId}`}>
+              <button className="room-btn">Play Game</button>
+            </Link>
+          ) : null}
+        </div>
+      </div>
     </div>
   );
 };
 
-export default JoinRoomPage;
+export default JoinRoom;
